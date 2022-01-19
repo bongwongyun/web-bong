2db076d27e45eb193a5871ca3fab6cc0a52ea55c
bongwongyun <97070046+SungilRyuu@users.noreply.github.com>
2022-01-19 18:49:39 +0900

    2022.01.19

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,11 +1,11 @@
-// 결과보기 버튼을 클릭하면 경고창의  띄워주세요
-document.querySelector(".result").addEventListener("click",function(){
-    alert("ddd")
-});
-document.querySelector(".result").addEventListener("click",()=>{});
+// 결과보기 버튼을 클릭하면 경고창의 띄워주세요
+//document.querySelector(".result").addEventListener("click",function(){
+//    alert("ddd")
+//});
+//document.querySelector(".result").addEventListener("click",()=>{});
 
 document.querySelectorAll(".result").forEach(function(el){
     el.addEventListener("click", function(){
-        alert("ddd")
-    })
+        el.classList.add("active")
+    });
 })
