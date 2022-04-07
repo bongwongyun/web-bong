d21591e8070d40390a23675af204c19100346157
bongwongyun <97070391+bongwongyun@users.noreply.github.com>
2022-04-07 21:26:05 +0900

    2022.04.07

---
+++ javascript/musicPlayer/apl/music.js
@@ -0,0 +1,62 @@
+var Allmusic = [
+    {
+        name:"Better Now",
+        artist: "Post Malone",
+        img: "포스트 말론 02",
+        src: "Post Malone Better Now"
+    },
+    {
+        name:"Circles",
+        artist: "Post Malone",
+        img: "포스트 말론 03",
+        src: "Post Malone Circles"
+    },
+    {
+        name:"rockstar",
+        artist: "Post Malone",
+        img: "포스트 말론 04",
+        src: "노래-3"
+    },
+    {
+        name:"Sunflower SpiderMan Into the SpiderVerse",
+        artist: "Post Malone Swae Lee",
+        img: "노래이미지-4",
+        src: "노래-4"
+    },
+    {
+        name:"I Feel It Coming",
+        artist: "The Weeknd&Daft Punk",
+        img: "포스트 말론 01",
+        src: "노래-5"
+    },
+    {
+        name:"Out of Time",
+        artist: "The Weeknd",
+        img: "노래이미지-6",
+        src: "노래-6"
+    },
+    {
+        name:"Save Your Tears",
+        artist: "The Weeknd",
+        img: "노래이미지-7",
+        src: "노래-7"
+    },
+    {
+        name:"Brave Shine",
+        artist: "Aimer",
+        img: "aimer01",
+        src: "노래-8"
+    },
+    {
+        name:"SPARK AGAIN",
+        artist: "Aimer",
+        img: "aimer02",
+        src: "노래-9"
+    },
+    {
+        name:"Better Now",
+        artist: "Post Malone",
+        img: "포스트 말론 02",
+        src: "Post Malone-Better Now"
+    }
+]
