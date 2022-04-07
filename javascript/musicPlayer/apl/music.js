95cf652cea17e94711d19b92f435f26648e7d269
bong <basaradog355@gmail.com>
2022-04-07 12:24:00 +0900

    2022.04.07

---
+++ javascript/musicPlayer/apl/music.js
@@ -0,0 +1,62 @@
+var allmusic = [
+    {
+        name:"Better Now",
+        artist: "Post Malone",
+        img: "노래이미지-1",
+        src: "노래-1"
+    },
+    {
+        name:"Circles",
+        artist: "Post Malone",
+        img: "노래이미지-2",
+        src: "노래-2"
+    },
+    {
+        name:"rockstar",
+        artist: "Post Malone",
+        img: "노래이미지-3",
+        src: "노래-3"
+    },
+    {
+        name:"Sunflower SpiderMan Into the SpiderVerse",
+        artist: "Post Malone Swae Lee",
+        img: "노래이미지-4",
+        src: "노래-4"
+    },
+    {
+        name:"I Feel It Coming",
+        artist: "The Weeknd&Daft Punk",
+        img: "노래이미지-5",
+        src: "노래-5"
+    },
+    {
+        name:"Out of Time",
+        artist: "The Weeknd",
+        img: "노래이미지-6",
+        src: "노래-6"
+    },
+    {
+        name:"Save Your Tears",
+        artist: "The Weeknd",
+        img: "노래이미지-7",
+        src: "노래-7"
+    },
+    {
+        name:"Brave Shine",
+        artist: "Aimer",
+        img: "노래이미지-8",
+        src: "노래-8"
+    },
+    {
+        name:"SPARK AGAIN",
+        artist: "Aimer",
+        img: "노래이미지-9",
+        src: "노래-9"
+    },
+    {
+        name:"잔향산가",
+        artist: "Aimer",
+        img: "노래이미지-10",
+        src: "노래-10"
+    }
+]
